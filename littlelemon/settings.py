b6fbebef563b0d2c8a764b97baaaa8c6ed8f5d65
SalCasalena <salcasalena@gmail.com>
2023-08-21 12:25:04 -0400

    Added API Support

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'reservation',
     'restaurant',
-    
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
